560bf49638acda3cc738916b5e6f280d4b78ba90
pengu2004 <u2208069@rajagiri.edu.in>
2025-09-20 10:33:42 +0530

    added logo and made the header white

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,19 @@
-import { AppBar, Toolbar, Typography, Container } from "@mui/material";
+import { AppBar, Toolbar, Typography, Container,Avatar } from "@mui/material";
 
 function Header() {
   return (
         <div>
-      <AppBar position="static" color="primary">
+      <AppBar position="static" sx={{ bgcolor: 'white' }}>
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Avatar alt="CoPirate Logo" src="/CoPirate_logo.png" sx={{ mr: 2, bgcolor: 'grey' }} />
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 , color: '#333',fontFamily:"Lato", fontWeight: 'bold'}}>
             CoPirate
           </Typography>
         </Toolbar>
       </AppBar>
 
       <Container sx={{ mt: 4, textAlign: "center" }}>
-        <Typography variant="h4" gutterBottom>
+        <Typography variant="h4" gutterBottom sx={{ color: '#333' }}> 
           Welcome to CoPirate 
         </Typography>
         <Typography variant="body1">

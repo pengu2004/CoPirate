f99b8cfb84661be2b016c1eaf6d2517b27b022e8
pengu2004 <u2208069@rajagiri.edu.in>
2025-09-28 21:27:18 +0530

    refactored to add backend

---
+++ backend/backend.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+
+app=FastAPI()
+
+@app.get("/")
+def welcome():
+    return "Hello World"
+
+@app.get("/twitter-stats/{username}")
+def get_twitter_stats(username):
+    from selenium import webdriver
+    from selenium.webdriver.chrome.service import Service
+    from webdriver_manager.chrome import ChromeDriverManager
+    from bs4 import BeautifulSoup
+
+
+    
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36") 
+
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service,options=options)
+
+    driver.get("http://nitter.net/" + username)
+    page_source = driver.page_source
+    driver.quit()
+    soup = BeautifulSoup(page_source, "html.parser")
+
+    data = soup.find_all("span", class_="profile-stat-num")
+    user_data = {}
+
+    Labels = ["Tweets", "Followers", "Following", "Likes"]
+
+    for i, label in enumerate(Labels):
+        user_data[label] = int(data[i].get_text().replace(",", ""))
+    return user_data

9d1cf4872a581a188c3abb6f43df17f6d6558c75
pengu2004 <u2208069@rajagiri.edu.in>
2025-10-01 11:38:52 +0530

    added hackerrank api

--- backend/backend.py
+++ backend/backend.py
@@ -47,3 +47,20 @@ def get_leetcode_stats(username):
     store=data.json()
     return (store["submitStats"]['acSubmissionNum'][0])
 
+@app.get("/hackerank-stats/{username}")
+def get_hackerank_stats(username):
+    """
+    api to get hackerank questions solved
+    """
+    import requests
+    headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+    }
+    data = requests.get("https://www.hackerrank.com/rest/hackers/" + username + "/badges", headers=headers)
+    store=data.json()
+    return store["models"][0]["solved"]
+
+
+
+
+
